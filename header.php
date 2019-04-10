537272712f8fe585afa11cdd244ec8fc1e3fd9ad
Scott Kennedy <scottyzen@gmail.com>
2019-04-10 22:19:05 +0100

    Updated font to Playfair Display

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 
